df5bf1654ae0d280062c672658861389c47b9505
Joshua Lorica <loricaj@hawaii.edu>
2023-11-01 15:17:04 -1000

    contacts collection finished in 32 minutes.

--- app/imports/ui/components/ContactAdmin.jsx
+++ app/imports/ui/components/ContactAdmin.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Image } from 'react-bootstrap';
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const Contact = ({ contact }) => (
+const ContactAdmin = ({ contact }) => (
   <Card className="h-100">
     <Card.Header>
       <Image src={contact.image} width={75} />
@@ -11,20 +11,22 @@ const Contact = ({ contact }) => (
     </Card.Header>
     <Card.Body>
       <Card.Text>{contact.description}</Card.Text>
+      <footer className="blockquote-footer">{contact.owner}</footer>
     </Card.Body>
   </Card>
 );
 
 // Require a document to be passed to this component.
-Contact.propTypes = {
+ContactAdmin.propTypes = {
   contact: PropTypes.shape({
     firstName: PropTypes.string,
     lastName: PropTypes.string,
     address: PropTypes.string,
     image: PropTypes.string,
     description: PropTypes.string,
+    owner: PropTypes.string,
     // _id: PropTypes.string,
   }).isRequired,
 };
 
-export default Contact;
+export default ContactAdmin;
